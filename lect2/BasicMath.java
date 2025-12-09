a1be85dad4d2c00239ff786576de40266fad95f6
himanshi singh <hsingh45968@gmail.com>
2025-12-09 20:56:46 +0530

    Basic

---
+++ lect2/BasicMath.java
@@ -0,0 +1,13 @@
+
+public class BasicMath {
+    public static void main(String[] args){
+        int a=10;
+        int b=4;
+        // basic operations
+        System.out.println("addition:"+(a+b));
+        System.out.println("subtraction :"+(a-b));
+        System.out.println("mutliplication :"+(a*b));
+        System.out.println("division:"+(a/b));
+        System.out.println("modulus:"+a%b);
+    }
+}
---
+++ lect2/TypeCasting.java
@@ -0,0 +1,12 @@
+//topic : conversion of int and double
+public class TypeCasting{
+    public static void main(String[] args){
+        int number =25;
+        double data =(int)number;
+        System.out.println("the value of double data is :"+number);
+        // conversion of float into double
+        float marks= 59.95f;
+        double value= (float) marks;
+        System.out.println("the value after conversion is :"+marks);
+    }
+}
