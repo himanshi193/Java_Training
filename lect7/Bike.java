ded79ba0f20af43dc7508d04b0455a9ef20bffed
himanshi singh <hsingh45968@gmail.com>
2025-12-11 21:55:54 +0530

    new

---
+++ lect7/Bike.java
@@ -0,0 +1,13 @@
+// classes and object
+public class Bike {
+    String name = "hiii";
+
+    void display() {
+        System.out.println(name);
+    }
+
+    public static void main(String[] args) {
+        Bike obj = new Bike();
+        obj.display(); //
+    }
+}
